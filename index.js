62d7ce8eb1f3c6d26a7d1f6f7f8c82ef7005ce99
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-03-19 18:24:10 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Code your solution!")
+let list = document.querySelector("form");
+list.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let pTag = document.querySelector("p");
+    let ul = document.querySelector("ul");
+    let listText = document.getElementById("list");
+    // let del = document.createElement("input");
+
+    if(listText.value) {
+        let newLi = document.createElement("li");
+        newLi.textContent = listText.value;
+        // del.type = "submit";
+        // del.value = "Delete"
+        ul.append(newLi);
+        pTag.textContent = "";
+    }
+    else {
+        pTag.textContent = "Error! Todo cannot be empty";
+    }
+    listText.value = "";
+
+    let oldList = document.querySelectorAll("li");
+    oldList.forEach(li => {
+        li.addEventListener("click", (event) => {
+            li.style.textDecoration = "line-through";
+        });
+    });
+});
